8f313e1cf613af907c720762c6bb2f710fc4bee1
LikaShu <142459648+LikaShu@users.noreply.github.com>
2023-12-28 16:28:23 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,43 @@
+    const data = generateRandomData();
+function generateRandomData() {
+main();
+    });
+
+
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+}
+        console.log("Random Number: " + item);
+function main() {
+    const data = [];
+    }
+}
+    return data;
+def generate_random_data():
+
+def main():
+    return data
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    for item in data:
+
+
+if __name__ == "__main__":def main():
+if __name__ == "__main__":
+    main()
+    for item in data:
+
+
+def generate_random_data():
+import random
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
